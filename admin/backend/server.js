1a6d5f78e20c39a4bb3410dd94d8bbb87ba4d59f
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-03-18 09:07:56 +0530

    bug fix

--- admin/backend/server.js
+++ admin/backend/server.js
@@ -32,13 +32,10 @@ connection.once("open",() => {
 
 //Routers Connection
 const serviceRouter=require("./routes/addservice");
-const staffallowance=require("./routes/addallowance");
-// const staffpaysalary=require("./routes/paysalary");
+
 
 //Routers
 app.use("/service",serviceRouter)
-app.use("/staffallowance",staffallowance)
-// app.use("/staffpaysalary",staffpaysalary)
 
 //Run on port
 app.listen(PORT, () => {
